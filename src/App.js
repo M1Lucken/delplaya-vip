0b75fecfc19b93128a3ed2505d30eed7c49b887e
Matt Luckenbihl <mluckenbihl@gmail.com>
2020-10-30 19:10:15 -0700

    move cname back to root directory

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import bg from './img/bg.jpg'
 function App() {
   return (
      <div styles={{ backgroundImage: `url(${bg})` }}>
-      
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Del Playa VIP Dev
+        </p>
+        </header>
+      </div>
      </div>
   );
 }
