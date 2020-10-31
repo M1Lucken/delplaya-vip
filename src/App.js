ffaebcf0d0c00df546e08fa760ec9975fbca762c
Matt Luckenbihl <mluckenbihl@gmail.com>
2020-10-30 19:08:32 -0700

    misc

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import bg from './img/bg.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Del Playa VIP Dev
-        </p>
-      </header>
-    </div>
+     <div styles={{ backgroundImage: `url(${bg})` }}>
+      
+     </div>
   );
 }
 
